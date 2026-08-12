16cfc146a82f25c822505712dc23ddcff4b8112f
Avip11-upes <avnindra.32372@stu.upes.ac.in>
2026-08-13 00:25:57 +0530

    remove day1q1 and day1q2 files

--- day1q1.c
+++
@@ -1,8 +0,0 @@
-//Write a program to input two numbers and display their sum.
-#include <stdio.h>
-void main(){
-    int a, b;
-    printf("Enter two numbers : ");
-    scanf("%d%d", &a, &b);
-    printf("Sum of %d and %d is %d", a, b, a+b);
-}
